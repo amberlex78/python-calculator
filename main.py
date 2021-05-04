9ab12a3be8cca898d7a41f1403bdde43017e77ff
Alexey Abrosimov <amberlex78@email.com>
2021-05-05 01:57:56 +0300

    run from 'main.py' file

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Form, Window = uic.loadUiType("calculator.ui")
 
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Your code here
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.exec_()
